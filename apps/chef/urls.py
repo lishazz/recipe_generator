c2cd1623118f9c1d12f0323bd556c1250eef842e
lishazz <lishamolsebastian@gmail.com>
2025-03-22 11:59:55 +0530

    update basic functions of user

--- apps/chef/urls.py
+++ apps/chef/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('chef_dashboard/', views.chef_dashboard, name='chef_dashboard'),
     path('create_recipe/',views.create_recipe,name="create_recipe"),
     path('view_review/',views.view_review,name="view_review"),
+    path('view_myrecipe/',views.view_myrecipe,name="view_myrecipe"),
 ]
--- apps/chef/views.py
+++ apps/chef/views.py
@@ -7,8 +7,19 @@ from apps.common.decorator import chef_required
 def chef_dashboard(request):
     return render(request, "chef_dashboard.html")
 
+
+@login_required
+@chef_required
 def create_recipe(request):
     return render(request,'create_recipe.html')
 
+
+@login_required
+@chef_required
 def view_review(request):
     return render(request,'view_review.html')
+
+@login_required
+@chef_required
+def view_myrecipe(request):
+    return render(request,"view_myrecipe.html")
