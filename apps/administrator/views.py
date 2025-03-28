2646d8ebd5dbcf61829da5fae148110d73420eb5
lishazz <lishamolsebastian@gmail.com>
2025-03-28 19:37:33 +0530

    added manage user feature

--- apps/administrator/views.py
+++ apps/administrator/views.py
@@ -9,13 +9,17 @@ from django.contrib import messages
 def administrator_dashboard(request):
     return render(request, "administrator_dashboard.html")
 
-
+@login_required
+@administrator_required
 def approve_chef(request):
     chefs = User.objects.filter(role='chef')  # Get all chefs
     return render(request, 'approve_chef.html', {'chefs': chefs})
 
+@login_required
+@administrator_required
 def manage_user(request):
-    return render(request,'manage_user.html')
+    users = User.objects.filter(role='generaluser')  # Get all chefs
+    return render(request,'manage_user.html',{'users': users})
 
 @login_required
 @administrator_required
@@ -26,3 +30,17 @@ def toggle_chef_status(request, user_id):
     status = "enabled" if chef.is_active else "disabled"
     messages.success(request, f"Chef {chef.username} has been {status}.")
     return redirect('approve_chef')  # Redirect to chef management page
+
+
+@login_required
+@administrator_required
+def toggle_user_status(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if user.is_active:
+        user.is_active = False
+        messages.success(request, f"User {user.username} has been disabled.")
+    else:
+        user.is_active = True
+        messages.success(request, f"User {user.username} has been enabled.")
+    user.save()
+    return redirect('manage_user')
